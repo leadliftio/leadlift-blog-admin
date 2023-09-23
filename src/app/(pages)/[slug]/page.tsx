2ecaf42a6f860293e87574ebaef0ffbebecd7a72
Kwesi Hervie <herviek2001@gmail.com>
2023-09-23 20:01:24 +0100

    ui changes

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -11,7 +11,7 @@ import { Hero } from '../../_components/Hero'
 import { generateMeta } from '../../_utilities/generateMeta'
 import { Page } from '../../../payload/payload-types'
 import { staticHome } from '../../../payload/seed/home-static'
-import HomePage from '../home/page'
+import serialize from '../../_components/RichText/serialize'
 
 // Payload Cloud caches all files through Cloudflare, so we don't need Next.js to cache them as well
 // This means that we can turn off Next.js data caching and instead rely solely on the Cloudflare CDN
@@ -42,10 +42,10 @@ export default async function Page({ params: { slug = 'posts' } }) {
   // if no `home` page exists, render a static one using dummy content
   // you should delete this code once you have a home page in the CMS
   // this is really only useful for those who are demoing this template
-  if (!page && slug === 'home') {
-    // page = staticHome
-    return HomePage()
-  }
+  // if (!page && slug === 'home') {
+  //   // page = staticHome
+  //   return serialize(page)
+  // }
 
   if (!page) {
     return notFound()
