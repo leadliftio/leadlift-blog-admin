ebbd91e7ada098e4bed43eb342ba2581ca1ae47c
Kwesi Hervie <herviek2001@gmail.com>
2023-09-25 07:10:50 +0100

    ui changes

--- src/app/(pages)/posts/[slug]/page.tsx
+++ src/app/(pages)/posts/[slug]/page.tsx
@@ -43,8 +43,8 @@ export default async function Post({ params: { slug } }) {
 
   return (
     <React.Fragment>
-      <div className="mt-[100px] max-w-[1200px] mx-auto px-[24px] lg:px-0">
-        <div className="w-[70%]">
+      <div className="mt-[75px] lg:mt-[100px] max-w-[1200px] mx-auto px-[24px] lg:px-0">
+        <div className="w-full lg:w-[70%]">
           <div className="mb-[30px]">
             <PostHero post={post} />
           </div>
