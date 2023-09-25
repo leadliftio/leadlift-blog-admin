92e7deab05853089095c084d29111a40b137b781
Kwesi Hervie <herviek2001@gmail.com>
2023-09-25 11:11:35 +0100

    ui changes

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+declare module '*.png'
+declare module '*.svg'
+declare module '*.jpeg'
+declare module '*.jpg'
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
